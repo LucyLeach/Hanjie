6538bf72c98abe5981878fede689b5e3c5094f85
Lucy <lucy@lucyleach.co.uk>
2015-01-31 08:16:29 +0000

    Remove unnecessary fields - will use Java 8's Optional rather than nullability annotations if they come back

--- src/main/java/uk/co/lucyleach/hanjie_solver/solver/UnsolvableException.java
+++ src/main/java/uk/co/lucyleach/hanjie_solver/solver/UnsolvableException.java
@@ -9,35 +9,18 @@ import uk.co.lucyleach.hanjie_solver.Puzzle;
  */
 public class UnsolvableException extends Exception
 {
-  //TODO add nullable annotations when you have access to the internet again
-  private final Integer row;
-  private final Integer column;
   private final Puzzle puzzle;
 
   public UnsolvableException(String message, int row, int column, Puzzle puzzle)
   {
     super("Error in row " + row + " and column " + column + ": " + message);
-    this.row = row;
-    this.column = column;
     this.puzzle = puzzle;
   }
 
   public UnsolvableException(String message, Puzzle puzzle)
   {
     super(message);
-    this.row = null;
-    this.column = null;
     this.puzzle = puzzle;
-  }
-
-  public Integer getRow()
-  {
-    return row;
-  }
-
-  public Integer getColumn()
-  {
-    return column;
   }
 
   public Puzzle getPuzzle()

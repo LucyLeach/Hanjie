c05013d2168ae3fa7e760484bdf6179afd1b10cd
Lucy <lucy@lucyleach.co.uk>
2015-02-07 11:22:15 +0000

    Log out when initial solutions creator is called

--- src/main/java/uk/co/lucyleach/hanjie_solver/performance/PerformanceAspect.java
+++ src/main/java/uk/co/lucyleach/hanjie_solver/performance/PerformanceAspect.java
@@ -3,7 +3,10 @@ package uk.co.lucyleach.hanjie_solver.performance;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * User: Lucy
@@ -13,6 +16,8 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class PerformanceAspect
 {
+  private final AtomicInteger counter = new AtomicInteger(0);
+
   @Around("allTopLevelMethods()")
   public Object timeProfile(ProceedingJoinPoint thisJoinPoint)
       throws Throwable
@@ -23,6 +28,13 @@ public class PerformanceAspect
     double timeTaken = (endTime - startTime) / 1000.;
     System.out.println("Time taken on " + getMethodName(thisJoinPoint) + ": " + timeTaken + "secs");
     return returnedObject;
+  }
+
+  @Before("initialSolutionsCreator()")
+  public void logCallCount()
+  {
+    int nextValue = counter.incrementAndGet();
+    System.out.println("Creating initial solutions, call number: " + nextValue);
   }
 
   private static String getMethodName(ProceedingJoinPoint thisJoinPoint)
@@ -49,5 +61,9 @@ public class PerformanceAspect
 
   @Pointcut("execution(* *..*PuzzleWriter_Spreadsheet.writePuzzle(..))")
   private void writer()
+  {}
+
+  @Pointcut("execution(* *..*InitialSolutionsCreator.create(..))")
+  private void initialSolutionsCreator()
   {}
 }
